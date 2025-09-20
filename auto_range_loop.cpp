bdeb1d86d6701802cce86d9024c9ee29b79623c4
PYadav70 <technoyadav1234@gmail.com>
2025-09-20 12:45:24 +0530

    Add commented-out increment operation in tempCodeRunnerFile.cpp

--- auto_range_loop.cpp
+++ auto_range_loop.cpp
@@ -10,4 +10,32 @@ int main(){
     }
     cout << endl;
 
+    // vector<int> :: iterator it;
+    for(auto it = v.begin(); it!=v.end(); it++){
+        cout<<(*it) << " ";
+    }
+
+    vector<pair<int,int>> v_p = {{1,2},{2,3}};
+    for(auto &value : v_p){
+        cout << value.first << " " << value.second << endl;
+    }
+
+    // for(int &value: v){
+    //     value++;
+    //     // cout << value  << " ";
+    // }
+
+    // for(int value: v){
+    //     // value++;
+    //     cout << value  << " ";
+    // }
+
+
+    // auto keyword
+
+    auto a = 1;
+    
+
+    cout << endl;
+
 }
